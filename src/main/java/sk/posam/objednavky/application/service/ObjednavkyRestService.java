278842e21b31bb7621ebcdd7de7f2c78c1738810
jasek <jasek@JASEK-NTB.posam.sk>
2018-10-05 13:50:13 +0200

    Stub na implementaciu s chybami

--- src/main/java/sk/posam/objednavky/application/service/ObjednavkyRestService.java
+++ src/main/java/sk/posam/objednavky/application/service/ObjednavkyRestService.java
@@ -2,16 +2,19 @@ package sk.posam.objednavky.application.service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import sk.posam.objednavky.domain.objednavka.Objednavka;
 import sk.posam.objednavky.domain.objednavka.ObjednavkyNaDen;
 import sk.posam.objednavky.domain.objednavka.ObjednavkyNaDenRepository;
 import sk.posam.objednavky.domain.objednavka.TerminObjednavky;
 import sk.posam.objednavky.domain.objednavka.TerminObjednavkyFactory;
+import sk.posam.objednavky.domain.osoba.Osoba;
 
 /**
  * Služba, ktorá implementuje restové API. Táto trieda neobsahuje žiadnu
@@ -51,10 +54,32 @@ public class ObjednavkyRestService {
 		return "fakt neviem"; // TODO toto je bug, treba to fixnut
 	}
 
-	// http://localhost:8080/objednavka/objednanyKedy?meno="Niels"&priezvisko="Bohr"
+	// http://localhost:8080/objednavka/objednanyKedy?meno=Enrico&priezvisko=Fermi
 	@RequestMapping(method=RequestMethod.GET, value="objednanyKedy")
 	public LocalTime kedyJeObjednany( String meno, String priezvisko ) {
-		return LocalTime.now(); // TODO toto je bug, treba to fixnut
+		final Osoba osoba = new Osoba( meno, priezvisko );
+		ObjednavkyNaDen objednavky = repository.findByDatum( LocalDate.now() );
+	 	Optional<Objednavka> objednavka = objednavky.najdiOsobu( osoba );
+		return objednavka.isPresent() ? objednavka.get().getTerminObjednavky().getCas() : null;
+	}
+	
+	// http://localhost:8080/objednavka/objednaj?meno=Enrico&priezvisko=Fermi
+	@RequestMapping(method=RequestMethod.POST, value="objednaj")
+	public Objednavka createObjednavkaNaDnes( String meno, String priezvisko ) {
+		final Osoba osoba = new Osoba( meno, priezvisko );
+		final LocalDate den = LocalDate.now();
+		Objednavka result;
+		
+		ObjednavkyNaDen objednavky = repository.findByDatum( den );
+		if ( objednavky == null ) {
+			objednavky = new ObjednavkyNaDen( den );
+			result = objednavky.objednaj( osoba );
+			repository.create( objednavky );
+		} else {
+			result = objednavky.objednaj( osoba );
+			repository.update( objednavky );
+		}
+		return result;
 	}
 	
 	// http://localhost:8080/objednavka
